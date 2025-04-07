15478217eae52c07d9a65632a56d5b75bd44e47f
xPeski <alrodr12@ucm.es>
2025-04-07 16:10:18 +0200

    Actualización: ajustes para Railway

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool(
   process.env.DATABASE_URL
     ? {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_PUBLIC_URL,
         ssl: {
           rejectUnauthorized: false,
         },
